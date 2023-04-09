3912152d02d36f9ac2839268e27db97b050b3614
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-04-10 02:18:29 +0600

    featured jobs added

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './FeaturedJobs.css'
+const FeaturedJobs = ({ jobInfo }) => {
+    return (
+        <div className=" w-full bg-slate-100 border rounded-lg">
+            <figure className="px-10 pt-10">
+                <img src={jobInfo.logo} alt="" className="rounded-xl logo" />
+            </figure>
+            <div className="ml-10">
+                <h2 className="jobs-heading mt-3">{jobInfo.job_title}</h2>
+                <p className='jobs-paragraph'>{jobInfo.company_name}</p>
+                <div className='flex flex-row gap-3 mt-3'>
+                    <button className='text-outlined color'>{jobInfo.remote_or_onsite}</button>
+                    <button className='text-outlined color'>{jobInfo.full_time_or_part_time}</button>
+                </div>
+
+                <div className='location-salary gap-6 mt-4 mb-6 flex flex-row'>
+
+                    <div className='gap-2 flex flex-row'>
+                        <img className='loco-logo' src={jobInfo.location_logo} alt="" />
+                        <p>{jobInfo.location}</p>
+                    </div>
+
+                    <div className='gap-2 flex flex-row'>
+                        <img className='salary-logo' src={jobInfo.frame} alt="" />
+                        <p>Salary: {jobInfo.location}</p>
+                    </div>
+                </div>     
+                <button className='btn mb-4 mt-auto'>View Details</button>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './Home.css'
-import { useLoaderData } from 'react-router-dom';
+// import { useLoaderData } from 'react-router-dom';
 import Job from '../Job/Job';
+import FeaturedJobs from '../FeaturedJobs/FeaturedJobs';
 const Home = () => {
-    // const { jobs } = useLoaderData()
-    // console.log(jobs)
+
     const [jobs, setJobs] = useState([])
+    const [jobState, setJobState] = useState([]);
     useEffect(() => {
         fetch('jobs.json').then(res => res.json()).then(data => setJobs(data))
     }, [])
+
+    useEffect(() => {
+        fetch('featured.json').then(res => res.json()).then(data => setJobState(data))
+    }, []);
+
+
     return (
         <React.Fragment>
-
             {/*heading, image */}
             <section className='flex justify-around bg-slate-100 pt-10'>
 
@@ -26,17 +32,34 @@ const Home = () => {
                 <img src="/src/assets/P3OLGJ1 copy 1.png" alt="" />
             </section>
 
-            {/*Job Category List */}
+            {/*Job Category List start*/}
             <section>
-                <h4 className='job-heading'>Job Category List</h4>
+                <p className='job-heading'>Job Category List</p>
                 <p className='paragraph text-center mt-4 mb-8'>Discover tens of thousands of employment options with the necessary knowledge.</p>
 
+                {/**card */}
                 <div className="my-container">
-                    <div className='grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2'>
+                    <div className='grid mb-8 mx-32 gap-32 lg:grid-cols-4 sm:grid-cols-2'>
                         {jobs.map(job => (<Job job={job} />))}
                     </div>
                 </div>
             </section>
+            {/*Job Category List end */}
+
+
+            {/*Featured Jobs start */}
+            <section>
+                <p className='job-heading'>Featured Jobs</p>
+                <p className='paragraph text-center mt-4 mb-8'>Discover tens of thousands of employment options with the necessary knowledge.</p>
+
+                {/**card */}
+                <div className="my-container">
+                    <div className='grid mb-4 mx-16 gap-16 lg:grid-cols-2 sm:grid-cols-1'>
+                        {jobState.map(jobInfo => (<FeaturedJobs key={jobInfo.id} jobInfo={jobInfo}></FeaturedJobs>))}
+                    </div>
+                </div>
+            </section>
+            {/*Featured Jobs end */}
         </React.Fragment>
     );
 };

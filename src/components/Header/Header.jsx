6ed68d37f454837693d7b19384ca6042205dd59c
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-04-09 22:34:20 +0600

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-
-        
-            <Link to='/'>Home</Link>
-            <Link to='/statistics'>Statistics</Link>
-            <Link to='/appliedJobs'>AppliedJobs</Link>
-            <Link to='/blog'>Blog</Link>
-        </div>
+        <nav className='flex justify-around bg-slate-100'>
+            <div className='text-2xl font-bold color'>
+                <h1>Job Explorer</h1>
+            </div>
+            <div className='text-xl font-semibold'>
+                <Link className='color' to='/'>Home</Link>
+                <Link className='color' to='/statistics'>Statistics</Link>
+                <Link className='color' to='/appliedJobs'>Applied Jobs</Link>
+                <Link className='color' to='/blog'>Blog</Link>
+            </div>
+            <div>
+            <button className='btn'>Start Applying</button>
+            </div>
+        </nav>
     );
 };
 
